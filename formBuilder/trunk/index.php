7dc9414c5e7068cb5251af0b3bb547d38f1c9f96
fmlheureux <fmlheureux@74b841b9-b9c5-464e-8fa9-dbe0ab4258d3>
2010-06-09 19:47:30 +0000

    Fixed display issue

--- formBuilder/trunk/index.php
+++ formBuilder/trunk/index.php
@@ -154,7 +154,7 @@ require_once("myFunctions.php");
 				</tfoot>
 			</table>
 		</div>
-		<div id="piton5" class="structure_value_domainsDiv create" style="border-style: solid; white-space: normal; overflow: scroll;">
+		<div id="piton5" class="structure_value_domainsDiv create" style="border-style: solid; white-space: normal; overflow: auto;">
 			<table class="insert ui-widget ui-widget-content">
 				<thead class="custom autoBuild1">
 					<tr class='ui-widget-header'>
