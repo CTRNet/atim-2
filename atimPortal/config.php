1978e6842a1b29da866e996e69d3e61ea171f319
fmlheureux <fmlheureux@74b841b9-b9c5-464e-8fa9-dbe0ab4258d3>
2011-03-25 14:56:22 +0000

    Initial import. Portal made to install when multiple instances of ATiM are installed on the same server.

---
+++ atimPortal/config.php
@@ -0,0 +1,18 @@
+<?php
+$install_links = array(
+	"install 1"	=> "#",
+	"install 2"	=> "#",
+	"install 3"	=> "#"
+);
+
+$usefull_links = array(
+	"ATiM Québec"						=> "http://quebec.ctrnet-dev.ca/index.php",
+	"Documentation d'ATiM (anglais)"	=> "http://www.ctrnet.ca/mediawiki/index.php/Category:User_guide"
+);
+
+$main_logos = array(
+	array(
+		"src" => "logofr.gif",
+		"alt" => "Logo Hopital Général Juif"
+	)
+);
---
+++ atimPortal/index.php
@@ -0,0 +1,78 @@
+<?php 
+require_once('config.php');
+$matches = array();
+$is_old_ie = null;
+preg_match('/MSIE\ [0-9]\.[0-9]/', $_SERVER['HTTP_USER_AGENT'], $matches);
+if(!empty($matches)){
+	$is_ie = true;
+	$version = substr($matches[0], 5) + 0;
+	$is_old_ie = $version < 8; 
+}else{
+	$is_old_ie = false;
+}
+?>
+
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />		
+		<title>Portail ATiM</title>
+	</head>
+	<body>
+		<div class='outer'>
+			<div class='inner'>
+				<?php 
+				$template = '<img src="%s" alt="%s"/>';
+				foreach($main_logos as $main_logo){
+					printf($template, $main_logo['src'], $main_logo['alt']);
+				} 
+				?>
+				<h1>Portail ATiM</h1>
+				<?php 
+				if($is_old_ie){
+					$template = '<a href="%s">%s</a>';
+					$line_parts = array();
+					foreach($install_links as $name => $url){
+						$line_parts[] = sprintf($template, $url, $name);
+					}
+					echo "<p>",implode(" | ", $line_parts),"</p>";
+				}else{
+				?>
+				<ul class="installLinks">
+					<?php 
+					$template = '<li><a href="%s">%s</a></li>';
+					foreach($install_links as $name => $url){
+						printf($template, $url, $name);
+					}
+					?>
+				</ul>
+				<?php 
+				}
+				?>
+				<img src="ctrnet_logo.png" alt="CTRNet logo" class="ctrnetLogo"/>
+				<div class="usefullLinks">
+					<h2>Liens utiles</h2>
+					<ul>
+					<?php 
+					$template = '<li><a href="%s">%s</a></li>';
+					foreach($usefull_links as $name => $url){
+						printf($template, $url, $name);
+					}	
+					?>
+					</ul>
+				</div>
+				<?php 
+				if($is_old_ie){
+					?>
+					<div class="oldIe">
+					ATTENTION: Votre version d'Internet Explorer est inférieure à 8 et risque de ne pas bien fonctionner avec ATiM.
+					</div>
+					<?php 
+				}
+				?>
+			</div>
+		</div>
+	</body>
+</html>
