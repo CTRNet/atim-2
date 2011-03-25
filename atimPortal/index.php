558f3ecc8b95d6dc23d8bfaad707cef440674b63
fmlheureux <fmlheureux@74b841b9-b9c5-464e-8fa9-dbe0ab4258d3>
2011-03-25 15:33:16 +0000

    favicon usage

--- atimPortal/index.php
+++ atimPortal/index.php
@@ -17,6 +17,7 @@ if(!empty($matches)){
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="shortcut icon" href="favicon.ico"/>
 		<link rel="stylesheet" type="text/css" href="style.css" />		
 		<title>Portail ATiM</title>
 	</head>
