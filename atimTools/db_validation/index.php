9806508b827210d4ece5956391d18f53469be792
nluc <nluc@74b841b9-b9c5-464e-8fa9-dbe0ab4258d3>
2014-04-23 17:31:22 +0000

    Add control linked to Issue #3039: Display of signs as percentage, lower than, etc in language_label, drop down list, etc

--- atimTools/db_validation/index.php
+++ atimTools/db_validation/index.php
@@ -663,6 +663,84 @@ foreach($grouped_forms as $grouped_form){
 	</tbody>
 </table>
 
+<h1>Strings requiring HTML code</h1>
+<table>
+	<thead>
+		<tr>
+			<th>structure</th>
+			<th>Model</th>
+			<th>Field</th>
+			<th>Used Label</th>
+			<th>i18n id</th>
+			<th>i18n en</th>
+			<th>i18n fr</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+	$query = "
+ 		SELECT alias as structure, model, field, 'structure_fields.language_label' AS 'structure_field', i18n.id AS i18n_id, en, fr 
+		FROM i18n 
+		INNER JOIN structure_fields ON structure_fields.language_label = i18n.id
+		INNER JOIN structure_formats ON structure_fields.id = structure_formats.structure_field_id
+		INNER JOIN structures ON structures.id = structure_formats.structure_id
+		WHERE ((fr LIKE '' OR en LIKE '') AND (i18n.id REGEXP '[<>]' OR (i18n.id REGEXP '%' AND i18n.id NOT REGEXP '%[a-zA-Z1-9\_]')))
+		OR (en REGEXP '[<>]' OR (en REGEXP '%' AND en NOT REGEXP '%[a-zA-Z1-9\_]'))
+		OR (fr REGEXP '[<>]' OR (fr REGEXP '%' AND fr NOT REGEXP '%[a-zA-Z1-9\_]'))
+		AND (structure_formats.flag_detail = 1 OR structure_formats.flag_index = 1)
+		AND structure_formats.flag_override_label != 1
+		UNION ALL
+		SELECT alias as structure, model, field, 'structure_fields.language_tag' AS 'structure_field', i18n.id AS i18n_id, en, fr 
+		FROM i18n 
+		INNER JOIN structure_fields ON structure_fields.language_tag = i18n.id
+		INNER JOIN structure_formats ON structure_fields.id = structure_formats.structure_field_id
+		INNER JOIN structures ON structures.id = structure_formats.structure_id
+		WHERE ((fr LIKE '' OR en LIKE '') AND (i18n.id REGEXP '[<>]' OR (i18n.id REGEXP '%' AND i18n.id NOT REGEXP '%[a-zA-Z1-9\_]')))
+		OR (en REGEXP '[<>]' OR (en REGEXP '%' AND en NOT REGEXP '%[a-zA-Z1-9\_]'))
+		OR (fr REGEXP '[<>]' OR (fr REGEXP '%' AND fr NOT REGEXP '%[a-zA-Z1-9\_]'))
+		AND (structure_formats.flag_detail = 1 OR structure_formats.flag_index = 1)
+		AND structure_formats.flag_override_tag != 1
+		UNION ALL
+		SELECT alias as structure, model, field, 'structure_formats.language_label' AS 'structure_field', i18n.id AS i18n_id, en, fr 
+		FROM i18n 
+		INNER JOIN structure_formats ON structure_formats.language_label = i18n.id
+		INNER JOIN structure_fields ON structure_fields.id = structure_formats.structure_field_id
+		INNER JOIN structures ON structures.id = structure_formats.structure_id
+		WHERE ((fr LIKE '' OR en LIKE '') AND (i18n.id REGEXP '[<>]' OR (i18n.id REGEXP '%' AND i18n.id NOT REGEXP '%[a-zA-Z1-9\_]')))
+		OR (en REGEXP '[<>]' OR (en REGEXP '%' AND en NOT REGEXP '%[a-zA-Z1-9\_]'))
+		OR (fr REGEXP '[<>]' OR (fr REGEXP '%' AND fr NOT REGEXP '%[a-zA-Z1-9\_]'))
+		AND (structure_formats.flag_detail = 1 OR structure_formats.flag_index = 1)
+		AND structure_formats.flag_override_label = 1
+		UNION ALL
+		SELECT alias as structure, model, field, 'structure_formats.language_tag' AS 'structure_field', i18n.id AS i18n_id, en, fr 
+		FROM i18n 
+		INNER JOIN structure_formats ON structure_formats.language_tag = i18n.id
+		INNER JOIN structure_fields ON structure_fields.id = structure_formats.structure_field_id
+		INNER JOIN structures ON structures.id = structure_formats.structure_id
+		WHERE ((fr LIKE '' OR en LIKE '') AND (i18n.id REGEXP '[<>]' OR (i18n.id REGEXP '%' AND i18n.id NOT REGEXP '%[a-zA-Z1-9\_]')))
+		OR (en REGEXP '[<>]' OR (en REGEXP '%' AND en NOT REGEXP '%[a-zA-Z1-9\_]'))
+		OR (fr REGEXP '[<>]' OR (fr REGEXP '%' AND fr NOT REGEXP '%[a-zA-Z1-9\_]'))
+		AND (structure_formats.flag_detail = 1 OR structure_formats.flag_index = 1)
+		AND structure_formats.flag_override_tag = 1
+		UNION ALL
+		SELECT alias as structure, model, field, 'structure_formats.language_heading' AS 'structure_field', i18n.id AS i18n_id, en, fr 
+		FROM i18n 
+		INNER JOIN structure_formats ON structure_formats.language_heading = i18n.id
+		INNER JOIN structure_fields ON structure_fields.id = structure_formats.structure_field_id
+		INNER JOIN structures ON structures.id = structure_formats.structure_id
+		WHERE ((fr LIKE '' OR en LIKE '') AND (i18n.id REGEXP '[<>]' OR (i18n.id REGEXP '%' AND i18n.id NOT REGEXP '%[a-zA-Z1-9\_]')))
+		OR (en REGEXP '[<>]' OR (en REGEXP '%' AND en NOT REGEXP '%[a-zA-Z1-9\_]'))
+		OR (fr REGEXP '[<>]' OR (fr REGEXP '%' AND fr NOT REGEXP '%[a-zA-Z1-9\_]'))
+		AND (structure_formats.flag_detail = 1 OR structure_formats.flag_index = 1);";
+	$result = $db->query($query) or die("ERR AT LINE ".__LINE__.": ".$db->error);
+	while($row = $result->fetch_assoc()){
+		printf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', $row['structure'], $row['model'], $row['field'], $row['structure_field'], $row['i18n_id'], $row['en'], $row['fr']);
+	}
+	$result->free();
+	?>
+	</tbody>
+</table>
+
 <h1>Strings requiring translation</h1>
 <div id="tr_target"></div>
 <table id="mt"><tr>
