af646d0184624d2e26ae8e2598d460ae06f0b750
nluc <nluc@74b841b9-b9c5-464e-8fa9-dbe0ab4258d3>
2018-09-14 16:11:05 +0000

    Change config

--- atimPortal/config.php
+++ atimPortal/config.php
@@ -1,32 +1,31 @@
 ﻿<?php
 $install_links = array(
-	"ATiM-Bank1"	=> "https://localhost/ATiM_bk1/",
-	"ATiM-Bank2"	=> "https://localhost/ATiM_bk2/",
-	"ATiM-Bank3"	=> "https://localhost/ATiM_bk3/"
+	"ATiM-Bank1"	=> "https://localhost/ATiM_bk1",
+	"ATiM-Bank2"	=> "https://localhost/ATiM_bk2",
+	"ATiM-Bank3"	=> "https://localhost/ATiM_bk3"
 );
 
 $usefull_links_and_data = array(
 	"ATiM Documentation / Documentation d'ATiM"	=> array(
-		"User Guide / Mode d'emploi (anglais)" => "http://www.ctrnet.ca/mediawiki/index.php/ATiM_User_Guide",
-		"ATiM Inventory Elements / Composants de l'inventaire (anglais)"	=> "http://www.ctrnet.ca/mediawiki/index.php/ATiM_Inventory_Elements",
-		"ATiM Inventory Elements Relationship / Liens des composants de l'inventaire (anglais)"	=> "http://www.ctrnet.ca/mediawiki/images/0/02/InventoryModelObjects.jpg",
-		"Databrowser : Help / Aide (anglais)"	=> "http://www.ctrnet.ca/mediawiki/index.php/Databrowser"
+		"ATiM Wiki : User Guide / Mode d'emploi (anglais)" => "http://www.ctrnet.ca/mediawiki/index.php/ATiM_User_Guide",
+	    "ATiM Training Video" => "https://biobanking.org/webs/atim_training_videos",
+	    "ATiM Demo" => "https://www.youtube.com/watch?v=vDJD1ytASdY&feature=youtu.be"
 	),
 	
 	"Project Documentation / Documentation du projet"	=> array(
-		"Basecamp: Project Management, Discussion, etc / Gestion du projet, discussions, etc" => "https://basecamp.com/2174200/projects/2298550-atim",
-		"Eventum: Bug & Requests Creation and Followup / Création et suivi des bugs et demandes" => "http://www.ctrnet-dev.ca/eventum/"
+		"Basecamp (Discussions, todo, bugs, etc / (Discussions, todo, bugs, etc)" => "Not Available"
 	),
 		
 	"Technical Support / Soutien techinique" => array(
 		"Serveur Data / Information sur le serveur" => array(
 			"IP" => "00.00.00.000", 
 			"Name / Nom" => "00000000000", 
-			"Administartor / Administrateur" => "James Bond"
+			"Administartor / Administrateur" => "N/A"
 		),
 		"Team / Équipe" => array(
-			"Nicolas Luc (CTRNet)" => "514-890-9000 # 25493 (nicol.luc@gmail.com)",
-			"Aaron Suggitt (CTRNet)" => "1-204-787-1416 (Aaron.Suggitt@ctrnet.ca)"
+			"Nicolas Luc (CTRNet - IT ATiM)" => "514-890-9000 # 25493 (nicol.luc@gmail.com)",
+			"Yaser Naderi (CTRNet - IT ATiM)" => "514-890-9000 # 31290 (yasernaderi5980@gmail.com)",
+			"Rebecca Barnes (CTRNet - Program Leader)" => "n/a (rebecca.barnes@ctrnet.ca )"
 		) 
 	)
 );
